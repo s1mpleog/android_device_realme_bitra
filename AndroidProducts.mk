0498b5e5e66dc28415b2e6269f1f2c30af7f3a40
RAKMO <rakmoparte@gmail.com>
2023-08-14 16:28:47 +0530

    Bringup Project Blaze

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_bitra.mk
+    $(LOCAL_DIR)/blaze_bitra.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_bitra-user \
-    bliss_bitra-userdebug \
-    bliss_bitra-eng
+    blaze_bitra-user \
+    blaze_bitra-userdebug \
+    blaze_bitra-eng
